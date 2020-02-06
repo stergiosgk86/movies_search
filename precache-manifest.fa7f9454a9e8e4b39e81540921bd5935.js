86e474663a8b5face46d180eb45e023680da3c1a
stergiosgk86 <52173282+stergiosgk86@users.noreply.github.com>
2020-02-06 15:03:54 +0200

    Updates

---
+++ precache-manifest.fa7f9454a9e8e4b39e81540921bd5935.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5028b5fafce07cf3f89b697122de7d56",
+    "url": "/movies_search/index.html"
+  },
+  {
+    "revision": "85a846bc593d1c15e4a4",
+    "url": "/movies_search/static/css/main.eea59b8f.chunk.css"
+  },
+  {
+    "revision": "6f6f67a9374a230b5d44",
+    "url": "/movies_search/static/js/2.3770613a.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/movies_search/static/js/2.3770613a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85a846bc593d1c15e4a4",
+    "url": "/movies_search/static/js/main.7ce92c4c.chunk.js"
+  },
+  {
+    "revision": "fb9bc3c5097d4adbcdc8",
+    "url": "/movies_search/static/js/runtime-main.e9d848ac.js"
+  }
+]);
